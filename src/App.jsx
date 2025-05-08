f2e153a5c7cf1a826387e59a623210a84418b8a2
prabodha <prabodharg1954@gmail.com>
2025-05-08 12:49:32 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>"Movie Explorer – Discover Your Favorite Films"</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
